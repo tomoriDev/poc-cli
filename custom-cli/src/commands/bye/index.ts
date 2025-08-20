52de258cd552a22bbc3487d44e5fe16d149578cf
Carlos Torres <ctorresm996@gmail.com>
2025-08-20 17:51:40 -0500

    feat(bye): enhance goodbye command with interactive prompts for target and manner

--- custom-cli/src/commands/bye/index.ts
+++ custom-cli/src/commands/bye/index.ts
@@ -2,18 +2,51 @@ import {Command} from '@oclif/core'
 import inquirer from 'inquirer'
 
 export default class Bye extends Command {
-  static description = 'Says goodbye to a selected person using inquirer'
+  static description = 'Says goodbye after asking a series of questions'
 
   public async run(): Promise<void> {
-    const response = await inquirer.prompt([
+    const answers = await inquirer.prompt([
       {
         choices: ['World', 'Friend', 'Everyone'],
         message: 'Who would you like to say goodbye to?',
-        name: 'name',
+        name: 'target',
         type: 'list',
+      },
+      {
+        choices: ['Formally', 'Casually', 'Loudly'],
+        message: 'How should we say it?',
+        name: 'manner',
+        type: 'list',
+      },
+      {
+        message: 'Are you sure you want to proceed?',
+        name: 'confirm',
+        type: 'confirm',
       },
     ])
 
-    this.log(`goodbye ${response.name}!`)
+    if (answers.confirm) {
+      let message = ''
+      switch (answers.manner) {
+        case 'Casually': {
+          message = `See ya, ${answers.target}!`
+          break
+        }
+
+        case 'Formally': {
+          message = `Goodbye, ${answers.target}. It was a pleasure.`
+          break
+        }
+
+        case 'Loudly': {
+          message = `GOODBYE, ${answers.target.toUpperCase()}!!!`
+          break
+        }
+      }
+
+      this.log(message)
+    } else {
+      this.log('Operation cancelled.')
+    }
   }
 }
